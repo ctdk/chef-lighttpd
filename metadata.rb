dcfcdb37177bc69a6c0a6628b22847d47ad3a9a6
ctdk <jbingham@gmail.com>
2014-12-07 12:04:32 -0800

    bump version number -- some things have been added since the last time the version number was bumped

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "jeremy@dailykos.com"
 license          "Apache 2.0"
 description      "Installs/Configures Lighttpd"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.3.0"
+version          "0.3.1"
 
 recipe "lighttpd::default", "default recipe"
 recipe "lighttpd::mod_auth", "mod_auth recipe"
